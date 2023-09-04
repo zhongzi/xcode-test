4b0b90000daa0bd8be33191fc8bd943265278cf1
中仔 <zhongzigm@gmail.com>
2023-09-04 23:34:37 +0800

    Initial Commit

---
+++ com.digitcollege.todolist/com_digitcollege_todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_digitcollege_todolistApp.swift
+//  com.digitcollege.todolist
+//
+//  Created by zhongzi on 2023/9/4.
+//
+
+import SwiftUI
+
+@main
+struct com_digitcollege_todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
